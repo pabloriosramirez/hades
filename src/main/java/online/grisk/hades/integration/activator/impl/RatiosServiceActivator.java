87223ab3d43020cd0b6ddbbfc537a85d282b7220
John <jonathan.pereira.ing@gmail.com>
2019-08-10 21:38:52 -0400

    💩

--- src/main/java/online/grisk/hades/integration/activator/impl/RatiosServiceActivator.java
+++ src/main/java/online/grisk/hades/integration/activator/impl/RatiosServiceActivator.java
@@ -10,19 +10,14 @@ import java.util.Map;
 import java.util.Random;
 
 @Component
-public class ScoreServiceActivator implements Activator {
+public class RatiosServiceActivator implements Activator {
 
     public Map<String, Object> invoke(@Payload Map<String, Object> payload, @Headers Map<String, Object> header) {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         Map<String, Object> attribute = new HashMap<>();
         for (int i = 0; i < new Random().nextInt(35) + 20; i++) {
-            attribute.put("score_" + i, new Random().nextInt(35000) + 1 + "");
+            attribute.put("variable_" + i, new Random().nextInt(35000) + 1 + "");
         }
-        payload.put("scoreRisk", attribute);
+        payload.put("dataIntegration", attribute);
         return payload;
     }
 }
