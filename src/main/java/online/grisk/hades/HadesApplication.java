9acf6563462e0679d9b72d9602f56452bf1afa1f
Pablo Rios Ramirez <pa.riosramirez@gmail.com>
2019-08-11 20:46:04 -0400

    Update HadesApplication.java

--- src/main/java/online/grisk/hades/HadesApplication.java
+++ src/main/java/online/grisk/hades/HadesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("integration.cfg.xml")
+@ImportResource("classpath:integration.cfg.xml")
 public class HadesApplication {
     public static void main(String[] args) {
         SpringApplication.run(HadesApplication.class, args);
